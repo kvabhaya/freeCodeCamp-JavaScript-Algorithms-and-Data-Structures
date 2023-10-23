1ae58c10b4ca904379cb8f44f0d221b2cd3dd290
Kokila Abhayasundara <94110759+kvabhaya@users.noreply.github.com>
2023-10-23 07:43:58 +0530

    Create Build JavaScript Objects.js

---
+++ Basic JavaScript/Build JavaScript Objects.js
@@ -0,0 +1,9 @@
+const myDog = {
+  // Only change code below this line
+  "name" : "Scooby",
+  "legs" : 4,
+  "tails" : 1,
+  "friends" : ["Human", "Cats"] 
+
+  // Only change code above this line
+};
