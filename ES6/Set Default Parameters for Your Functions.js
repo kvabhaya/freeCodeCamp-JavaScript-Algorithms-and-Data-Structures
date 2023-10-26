e5ddccef341279eb080cc4ea16f5c1b93ad42dde
Kokila Abhayasundara <94110759+kvabhaya@users.noreply.github.com>
2023-10-26 15:04:55 +0530

    Create Set Default Parameters for Your Functions.js

---
+++ ES6/Set Default Parameters for Your Functions.js
@@ -0,0 +1,9 @@
+// Only change code below this line
+
+function increment(number, value = 1) {
+    return number + value;
+}
+console.log(increment(5, 2)); // returns 7
+console.log(increment(5)); // returns NaN
+
+// Only change code above this line
